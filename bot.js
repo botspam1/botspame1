5346e6a7867891f3a51af8355b477477c9f3bb75
botspam1 <49961396+botspam1@users.noreply.github.com>
2019-04-24 08:27:58 -1200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("570702121117417475")
+setInterval(function() {
+channel.send(`i love you all`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
